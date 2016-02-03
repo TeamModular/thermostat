19b3206efa4d583928a9afb1efddeedbf329a2ad
Richard Lowe <b0ggyb33@gmail.com>
2016-02-03 18:55:45 +0000

    no idea

---
+++ Assets/Scripts/LightController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightController : MonoBehaviour {
+
+	UnityEngine.Component lightSource;
+	Light light;
+	// Use this for initialization
+	void Start () 
+	{
+		lightSource = GetComponent ("lightbulb");
+		light = this.lightSource.GetComponent<Light> ();
+	}
+
+	public void OnMouseClick()
+	{
+		Toggle ();
+	}
+
+	private void Toggle() 
+	{
+		
+		light.enabled = !light.enabled;
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
