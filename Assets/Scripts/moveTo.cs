ba74fb36e43de9540392ca1a837e113893831853
Richard Lowe <b0ggyb33@gmail.com>
2016-02-05 20:12:54 +0000

    created mesh from png using blender and added navmesh

---
+++ Assets/Scripts/moveTo.cs
@@ -0,0 +1,14 @@
+﻿// MoveTo.cs
+using UnityEngine;
+using System.Collections;
+
+public class moveTo : MonoBehaviour 
+{
+	public Transform goal;
+
+	void Start () 
+	{
+		NavMeshAgent agent = GetComponent<NavMeshAgent>();
+		agent.destination = goal.position; 
+	}
+}
