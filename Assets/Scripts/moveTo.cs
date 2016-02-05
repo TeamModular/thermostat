228c1e5a41fcd961e52119fe83b11207d47b8520
Richard Lowe <b0ggyb33@gmail.com>
2016-02-05 21:09:17 +0000

    messing around with Person move speed and new position update rate

--- Assets/Scripts/moveTo.cs
+++ Assets/Scripts/moveTo.cs
@@ -7,22 +7,33 @@ public class moveTo : MonoBehaviour
 	public Vector2 minimumLimits;
 	public Vector2 maximumLimits;
 
+	bool waiting;
+
 	NavMeshAgent agent;
 
 	void Start()
 	{
+		waiting = false;
 		agent = GetComponent<NavMeshAgent> ();
+	}
+
+	IEnumerator SetNewDestinationAfterWaiting(float time)
+	{
+		yield return new WaitForSeconds (2);
+
+		Vector3 location = new Vector3 (Random.Range (minimumLimits.x, maximumLimits.x), 0, Random.Range (minimumLimits.y, maximumLimits.y));
+		agent.destination = location; 
+		waiting = false;
 	}
 
 	void Update () 
 	{
-		if (agent.velocity == Vector3.zero) 
+		if (agent.velocity == Vector3.zero && !waiting) 
 		{
-			Vector3 location = new Vector3 (Random.Range (minimumLimits.x, maximumLimits.x), 0, Random.Range (minimumLimits.y, maximumLimits.y));
-			agent.destination = location; 
-		}
+			StartCoroutine (SetNewDestinationAfterWaiting (1));
+			waiting = true;
+		} 
 	}
-
 
 
 }
