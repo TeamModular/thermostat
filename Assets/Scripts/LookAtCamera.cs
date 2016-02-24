b2f6da3a1874c9c9abb86468548fff03684c2905
Michael Hodgson <michaelwhodgson@gmail.com>
2016-02-24 20:09:06 +0000

    Made happyness bars look at the camera at all times

---
+++ Assets/Scripts/LookAtCamera.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAtCamera : MonoBehaviour {
+    public Camera gameCamera;
+    private float initialRotation;
+
+    void Start()
+    {
+        initialRotation = transform.rotation.z;
+    }
+
+    void Update()
+    {
+        transform.rotation = gameCamera.transform.rotation;
+        transform.Rotate(Vector3.up * initialRotation * 180); // Fix Rotation (Flipped sprites)
+    }
+}
