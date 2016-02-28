5dbfefeb6f82ee3eda7b36c3c8025cbd5fd04345
Michael Hodgson <michaelwhodgson@gmail.com>
2016-02-28 14:19:36 +0000

    Added scene selection menu

---
+++ Assets/LevelLoader.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LevelLoader : MonoBehaviour {
+
+	public void LoadLevel(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
