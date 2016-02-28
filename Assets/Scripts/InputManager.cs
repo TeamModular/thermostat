da98adcd808de46ce4b618c7d5ab593d60d3aae1
Michael Hodgson <michaelwhodgson@gmail.com>
2016-02-28 12:37:55 +0000

    Added pause menu and basic GameManager script

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManager : MonoBehaviour {
+
+    public GameObject PauseCanvas;
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown("escape"))
+        {
+            GameManager.Instance.SetPause(!GameManager.Instance.Paused);
+            PauseCanvas.SetActive(GameManager.Instance.Paused);
+        }
+
+	}
+}
